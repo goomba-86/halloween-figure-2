d5812789a3ad16e73e2cd01f4e3330596b76ac74
Olli <olli.a.sjoberg@gmail.com>
2023-09-02 19:53:34 +0300

    Create motor controller and run it infinitely in main loop

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,25 @@ pub mod stepper_motor_controller;
 
 use crate::file_io::FileIOImpl;
 use crate::gpio::{Direction, RpiGpioController};
+use crate::stepper_motor_controller::StepperMotorController;
+use std::io::Result;
+use std::{thread, time};
 
-fn main() {
-    let gpio_controller = RpiGpioController::new(FileIOImpl {}, Direction::Out, 10);
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let pins = vec![
+        RpiGpioController::new(FileIOImpl {}, Direction::Out, 12).unwrap(),
+        RpiGpioController::new(FileIOImpl {}, Direction::Out, 16).unwrap(),
+        RpiGpioController::new(FileIOImpl {}, Direction::Out, 20).unwrap(),
+        RpiGpioController::new(FileIOImpl {}, Direction::Out, 21).unwrap(),
+    ];
+
+    let stepper_motor = StepperMotorController::new(pins, 5);
+    let turn_wait_milli_seconds = time::Duration::from_millis(3000);
+
+    loop {
+        stepper_motor.turn_degrees(30)?;
+        thread::sleep(turn_wait_milli_seconds);
+        stepper_motor.turn_degrees(-30)?;
+        thread::sleep(turn_wait_milli_seconds);
+    }
 }
